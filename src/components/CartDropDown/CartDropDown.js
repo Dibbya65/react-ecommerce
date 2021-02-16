61a16b0fc064661e1aa741bc5841114b66761d53
Dibbya <dibyakarki65@gmail.com>
2021-02-16 20:30:05 +1100

    reselect

--- src/components/CartDropDown/CartDropDown.js
+++ src/components/CartDropDown/CartDropDown.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Button from '../Button/Button';
 import CartItem from '../CartItem/CartItem';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import './CartDropDown.scss';
 
 const CartDropDown = ({ cartItems }) => {
@@ -16,8 +17,8 @@ const CartDropDown = ({ cartItems }) => {
     </div>
   );
 };
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropDown);
